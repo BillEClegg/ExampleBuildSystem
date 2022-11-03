893ea29b6dcfa8cd7309c52501b9c6d861c6a669
BillEClegg <115092418+BillEClegg@users.noreply.github.com>
2022-11-03 12:18:08 -0400

    Update labview_diff.py

--- steps/labview_diff.py
+++ steps/labview_diff.py
@@ -97,6 +97,7 @@ def get_changed_labview_files(target_ref):
     :return: Tuples of the form (status, filename) where status is either "A" or "M", depending on whether the file was added or modified.
     """
     diff_args = ["git.exe", "diff", "--name-status", "--diff-filter=AM", target_ref + "...", "HEAD"]
+    print(diff_args)
     diff_output = subprocess.check_output(diff_args).decode("utf-8")
 
     # https://regex101.com/r/EFVDVV/1
